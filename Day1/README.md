5caa5f762fd895bf8352563fcf85654e1c09958e
Rogger <rvasque3@gmail.com>
2020-04-26 19:34:52 -0500

    added button functionality and styles, day 1 finished

--- Day1/README.md
+++ Day1/README.md
@@ -1,3 +1,11 @@
 # Day 1 - Virtual Drumkit
 
 Creating a virtual drumkit manipulating the DOM and adding a transition when the key is pressed.
+
+Played with the styles and shadows to add 3 neumorphic buttons:
+
+![Button State](button.png)
+
+It doesn't look like a drumset but it sounds like one. :laughing:
+
+> Sounds samples thanks to Sampleswap.org
--- Day1/index.js
+++ Day1/index.js
@@ -1,7 +1,21 @@
-window.addEventListener('keydown', e => {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   if (!audio) return //Stop the function from running where there is no audio
   audio.currentTime = 0 //Rewind to the start to allow multiples key/audio
   audio.play()
+  key.classList.add('pressed')
+}
+
+window.addEventListener('keydown', playSound)
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return // Skip if its not a transform
+  this.classList.remove('pressed')
+}
+
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition)
 })
